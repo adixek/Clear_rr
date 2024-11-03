1143517d791b5239ec95d1242753b85f6f51a7fe
adixek <kaz14adi@gmail.com>
2024-11-03 16:05:22 +0530

    Adixek patch 1 (#1)
    * Delete .github/workflows directory
    
    * Delete docs directory
    
    * Delete update.json
    
    * Delete CHANGELOG.md
    
    * Delete build.sh
    
    * Update cleaner
    
    Specified launcher3 only
    
    * Update cleaner.service
    
    * Update module.prop
    
    * Update cleaner.service

--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-zip -r -o -X -ll CCforKSUM-$(cat module.prop | grep 'version=' | awk -F '=' '{print $2}').zip ./ -x '.git/*' -x 'CHANGELOG.md' -x 'update.json' -x 'build.sh' -x '.github/*' -x 'docs/*' -x 'README.md'
